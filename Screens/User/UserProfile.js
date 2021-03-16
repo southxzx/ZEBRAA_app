a857ef041d882cb78c1e29bf46a2bbf3f06d9baf
southxz <phuongnamsk109@gmail.com>
2021-03-16 16:59:08 +0700

    16 Login

---
+++ Screens/User/UserProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const UserProfile = () => {
+    return (
+        <View>
+            <Text>User profile</Text>
+        </View>
+    )
+}
+
+export default UserProfile;
